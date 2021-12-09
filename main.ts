8dbee773c814a5c72fa53569e58f4c7c82c0d497
tkhk4649 <95830913+tkhk4649@users.noreply.github.com>
2021-12-09 09:11:05 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,13 @@
-radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == 1) {
-        mode = 0
-        sended = 0
-        recieved = 1
-        basic.showString("passed")
-    } else {
-    	
-    }
-})
 input.onButtonPressed(Button.A, function () {
     if (mode == 0) {
         mode = 1
-        sended = 1
-        recieved = 0
-        radio.sendNumber(1)
+        radio.sendValue("mode", 1)
         basic.showString("Sending")
     }
 })
-let recieved = 0
-let sended = 0
+radio.onReceivedString(function (receivedString) {
+	
+})
 let mode = 0
 radio.setGroup(1)
 basic.forever(function () {
